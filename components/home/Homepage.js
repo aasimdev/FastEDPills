dbecbde0c242064abb1ef2136db6f3202e1cc368
aasimdev <aasimdev69@gmail.com>
2023-01-25 22:25:23 +0500

    issue

--- components/home/Homepage.js
+++ components/home/Homepage.js
@@ -3,11 +3,13 @@ import AboutTreatment from './AboutTreatment'
 import TreatmentTable from './TreatmentTable'
 import Treatments from './Treatments'
 import TreatmentQNA from './TreatmentQNA'
+import { useRouter } from 'next/router'
 
 const Homepage = () => {
+    const router = useRouter();
     return (
         <div className='mx-auto px-4 md:px-12 xl:px-0 xl:max-w-screen-xl'>
-            <div className='sm:bg-white md:mt-12 pt-7 sm:py-8 sm:px-4 sm:border border-gray mb-14'>
+            <div className={`sm:bg-white pt-7 sm:py-8 sm:px-4 sm:border border-gray mb-14 ${router.asPath !== "/about-us" ? "md:mt-12" : ""}`}>
                 {/* About Treatments */}
                 <AboutTreatment />
 
